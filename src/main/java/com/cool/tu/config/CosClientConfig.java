1132807eb5548548390da8994adb7cac2ede4741
Cool <990299103@qq.com>
2024-12-16 16:43:16 +0800

    feat:"上传图片功能开发"

---
+++ src/main/java/com/cool/tu/config/CosClientConfig.java
@@ -0,0 +1,40 @@
+package com.cool.tu.config;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSSClientBuilder;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "aliyun.oss")
+@Data
+public class CosClientConfig {
+  
+    /**  
+     * endpoint
+     */  
+    private String endpoint;
+  
+    /**  
+     * accessKeyId
+     */  
+    private String accessKey;
+
+    /**
+     * accessKeySecret
+     */
+    private String secretKey;
+
+    /**
+     * bucketName
+     */
+    private String bucketName;
+  
+    @Bean
+    public OSS ossClient() {
+        return new OSSClientBuilder().build(endpoint, accessKey, secretKey);
+    }  
+}
