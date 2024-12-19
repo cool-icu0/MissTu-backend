72edb6f6205b478c68f8bbf49cd650e163768d82
Cool <990299103@qq.com>
2024-12-19 17:28:04 +0800

    feat:"开发用户传图和管理员批量传图功能"

--- src/main/java/com/cool/tu/model/dto/picture/PictureUploadRequest.java
+++ src/main/java/com/cool/tu/model/dto/picture/PictureUploadRequest.java
@@ -13,5 +13,15 @@ public class PictureUploadRequest implements Serializable {
      */
     private Long id;
 
+    /**
+     * 图片名称
+     */
+    private String picName;
+
+    /**
+     * 文件地址
+     */
+    private String fileUrl;
+
     private static final long serialVersionUID = 1L;
 }
